a9faa485f0578c355492b74457dda0b0b2b3025f
David Spriggs <dragoncrane65@gmail.com>
2023-08-09 23:38:01 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var members = document.getElementById("members");
+    var skillsMember = document.getElementById("skillsMember");
+    var membersMember = document.getElementById("membersMember");
+    skills.style.display = "block";
+    members.style.display = "none";
+    skillsMember.style.backgroundColor = "#fff";
+    membersMember.style.backgroundColor = "#f5f5f5";
+}
